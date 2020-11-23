2f1dd795a7cf5c591865a1e21b38660c9afeebb1
josoriom <javier.osorio@correounivalle.edu.co>
2020-11-22 23:23:49 -0500

    fix: eslint

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,9 +1,10 @@
 import DirectManager from '..';
+
 import prediction from './prediction/test.json';
 
 describe('test myModule', () => {
   const directManager = new DirectManager(prediction);
-  it('It should extract parameters', () => {
+  it('Extract parameters', () => {
     const parameters = directManager.getParameters();
     expect(parameters[0]).toStrictEqual({
       delta: 2.611,
@@ -18,13 +19,13 @@ describe('test myModule', () => {
     });
   });
 
-  it('It should suggest boundaries twigTemplate', () => {
+  it('Suggest boundaries twigTemplate', () => {
     const boundaries = directManager.getBoundaries({ error: 0.2 });
     expect(boundaries.lower).toStrictEqual([2.411, 6.962, 0.75, 6.962]);
     expect(boundaries.upper).toStrictEqual([2.811, 7.362, 1.15, 7.362]);
   });
 
-  it('It should build boundaries', () => {
+  it('Should build boundaries', () => {
     const suggestedBoundaries = directManager.suggestBoundaries({ error: 0.2 });
     expect(suggestedBoundaries[0]).toStrictEqual({
       lowerDelta: 2.411,
